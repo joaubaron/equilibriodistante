3a1ef68c87ede3e3b1eaad25188ea457e913b315
joaubaron <joaubaron@users.noreply.github.com>
2026-05-07 12:43:06 +0000

    deploy: 146af7510bb9392adc1b99f7a376457fce997b8c

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = '07.05.2026-0938';
+const CACHE_VERSION = '07.05.2026-0943';
 const CACHE_STATIC  = `gente-static-${CACHE_VERSION}`;
 const CACHE_MP3     = `gente-mp3-${CACHE_VERSION}`;
 const BASE          = '/equilibriodistante';
