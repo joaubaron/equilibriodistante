845e1d07819268c9a0bda162b941be16dad6d419
joaubaron <joaubaron@users.noreply.github.com>
2026-05-08 15:59:11 +0000

    deploy: 4727e629fcb82062ced4d88479567cafed2e2508

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = '08.05.2026-1250';
+const CACHE_VERSION = '08.05.2026-1259';
 const CACHE_STATIC  = `gente-static-${CACHE_VERSION}`;
 const CACHE_MP3     = `gente-mp3-${CACHE_VERSION}`;
 const BASE          = '/equilibrio';
